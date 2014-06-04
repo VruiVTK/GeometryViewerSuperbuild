afee7c026980ced29b0cde7ebcd4b656df16366e
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2014-06-04 16:10:21 -0400

    Superbuild builds VRUI, VTK and VruiVTK

---
+++ Projects/VRUI.cmake
@@ -0,0 +1,23 @@
+# This script fetches and builds VRUI
+
+SET (VRUI_ROOT
+  ${CMAKE_BINARY_DIR}/VRUI
+  CACHE INTERNAL ""
+  )
+
+file(MAKE_DIRECTORY "${VRUI_ROOT}/pkgconfig")
+file(MAKE_DIRECTORY "${VRUI_ROOT}/bld")
+
+ExternalProject_Add (
+  VRUI
+  PREFIX "${VRUI_ROOT}"
+  STAMP_DIR "${VRUI_ROOT}/stamp"
+  URL "http://idav.ucdavis.edu/~okreylos/ResDev/Vrui/Vrui-3.1-002.tar.gz"
+  URL_HASH SHA1=3c4ded4ed18e4a16394cdc1d625d7d0f4f84b108
+  UPDATE_COMMAND ""
+  SOURCE_DIR "${VRUI_ROOT}/src"
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ${CMAKE_BUILD_TOOL} INSTALLDIR=${VRUI_ROOT}/bld PKGCONFIGINSTALLDIR=${VRUI_ROOT}/pkgconfig
+  INSTALL_COMMAND ${CMAKE_BUILD_TOOL} INSTALLDIR=${VRUI_ROOT}/bld PKGCONFIGINSTALLDIR=${VRUI_ROOT}/pkgconfig install
+  BUILD_IN_SOURCE 1
+  )
---
+++ Projects/VTK.cmake
@@ -0,0 +1,25 @@
+# This script fetches and builds VTK
+
+SET(VTK_ROOT
+  ${CMAKE_BINARY_DIR}/VTK
+  CACHE INTERNAL ""
+  )
+
+SET (VTK_CMAKE_ARGS
+  -DBUILD_TESTING:BOOL=OFF
+  -DBUILD_SHARED_LIBS:BOOL=OFF
+  -DBUILD_EXAMPLES:BOOL=OFF
+  -DModule_vtkRenderingExternal:BOOL=ON
+  )
+
+ExternalProject_Add(
+  VTK
+  PREFIX "${VTK_ROOT}"
+  STAMP_DIR "${VTK_ROOT}/stamp"
+  GIT_REPOSITORY "https://github.com/sankhesh/VTK.git"
+  GIT_TAG "Rendering_External_Module"
+  SOURCE_DIR "${VTK_ROOT}/src"
+  BINARY_DIR "${VTK_ROOT}/bld"
+  CMAKE_ARGS "${VTK_CMAKE_ARGS}"
+  INSTALL_COMMAND ""
+  )
