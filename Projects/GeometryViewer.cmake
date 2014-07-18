e36aeb8e95b97b02d6cb4054bce8ef75d42a8dcd
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2014-07-18 10:02:01 -0400

    Update to new name

---
+++ Projects/GeometryViewer.cmake
@@ -0,0 +1,25 @@
+# This script fetches and builds GeometryViewer
+
+SET(GeometryViewer_ROOT
+  "${CMAKE_BINARY_DIR}/GeometryViewer"
+  )
+
+SET(GeometryViewer_CMAKE_ARGS
+  -DVRUI_PKGCONFIG_DIR:PATH=${VRUI_ROOT}/pkgconfig
+  -DVTK_DIR:PATH=${VTK_ROOT}/bld
+  )
+
+ExternalProject_Add(
+  GeometryViewer
+  DEPENDS VRUI VTK
+  PREFIX "${GeometryViewer_ROOT}"
+  STAMP_DIR "${GeometryViewer_ROOT}/stamp"
+  GIT_REPOSITORY "https://github.com/VruiVTK/GeometryViewer.git"
+  GIT_TAG "master"
+  #  UPDATE_COMMAND ${GIT_EXECUTABLE} pull
+  SOURCE_DIR "${GeometryViewer_ROOT}/src"
+  BINARY_DIR "${GeometryViewer_ROOT}/bld"
+  CMAKE_ARGS ${GeometryViewer_CMAKE_ARGS}
+  INSTALL_COMMAND ""
+  )
+
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 VruiVTKSuperbuild
 =================
 
-Superbuild that builds all dependencies including VRUI, VTK and VruiVTK
+Superbuild that builds all dependencies including VRUI, VTK and GeometryViewer
 
-Detailed documentation and demos can be found at http://vruivtk.github.io/VruiVTKSuperbuild
+Detailed documentation and demos can be found at http://vruivtk.github.io/GeometryViewerSuperbuild
