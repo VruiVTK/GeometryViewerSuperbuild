a0b3badad8d9d0ad98d78f39d31d5e678b9033f8
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2015-04-10 16:11:58 -0400

    Fetch VTK from master

--- Projects/VTK.cmake
+++ Projects/VTK.cmake
@@ -11,15 +11,14 @@ SET (VTK_CMAKE_ARGS
   -DBUILD_SHARED_LIBS:BOOL=OFF
   -DBUILD_EXAMPLES:BOOL=OFF
   -DModule_vtkRenderingExternal:BOOL=ON
-  -DVTK_USE_X:BOOL=ON
   )
 
 ExternalProject_Add(
   VTK
   PREFIX "${VTK_ROOT}"
   STAMP_DIR "${VTK_ROOT}/stamp"
-  GIT_REPOSITORY "https://github.com/sankhesh/VTK.git"
-  GIT_TAG "Rendering_External_Module"
+  GIT_REPOSITORY "https://gitlab.kitware.com/vtk/vtk.git"
+  GIT_TAG "1866265f37c13a5065cacca8b65c7f089c6805e4"
   #  UPDATE_COMMAND ${GIT_EXECUTABLE} pull
   SOURCE_DIR "${VTK_ROOT}/src"
   BINARY_DIR "${VTK_ROOT}/bld"
